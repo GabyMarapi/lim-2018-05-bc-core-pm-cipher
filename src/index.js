ed37f887a6b3507878751542de75eb27bf87d133
GabyMarapi <gaby1017x@gmail.com>
2018-05-28 16:14:06 -0500

    mis cambios

--- src/index.js
+++ src/index.js
@@ -1,66 +1,43 @@
+//Declarando costantes
 const inputElement1 = document.getElementById('texto1');
 const inputElement2 = document.getElementById('texto2');
 const buttonCifrar = document.getElementById('cifrar');
 const buttonCifrar1 = document.getElementById('descifrar');
 
 
-// inputElement1.addEventListener('input' , ()=>{
-//     const texto1=inputElement1.value;
-//     const n=2;
-//     let show=''
-//     for (let i=0; i < texto1.length; i++){
-//     const x=texto1.charCodeAt(i);
-//     const r= (x- 65 + n)%26+65;
-//     show+=String.fromCharCode(r);
-//     }
-//  inputElement2.value=show;
-// })
+//Creamos la función de cifrado y decifrado
 
-
-
-inputElement1.addEventListener ('input', () =>{
-    texto1=inputElement1.value;
-    const n=33;
-    let show=''
-    for (let i=0; i < texto1.length; i++){
-    const x=texto1.charCodeAt(i);
-    const r= (x- 65 + n)%26+65;
-    show+=String.fromCharCode(r);
-    }
-inputElement2.value=show;
-})
 
 
 let texto1='';
 
-/* const cifrar = (texto1) =>{
-    
-   texto1=inputElement1.value;
-    //console.log(texto1)
-    const n=33;
+const cifrar = (texto1) =>{
+    texto1=inputElement1.value;
+    const offSet=prompt('Ingresa el OffSet para la decodificación');//obtener el offset
     let show=''
     for (let i=0; i < texto1.length; i++){
     const x=texto1.charCodeAt(i);
-    const r= (x- 65 + n)%26+65;
+    const r= (x- 65 + offSet)%26+65;
     show+=String.fromCharCode(r);
     }
     return inputElement2.value=show;
 }
 
-const cifrar1 = (texto1) =>{
+const descifrar = (texto1) =>{
     
     texto1=inputElement1.value;
-    //console.log(texto1)
-    const n=33;
+    const offSet=prompt('Ingresa el OffSet para la codificación');//obtener el offset
     let show=''
     for (let i=0; i < texto1.length; i++){
     const x=texto1.charCodeAt(i);
-    const r= (x+ 65 - n)%26+65;
+    const r= (x+ 65 - offSet)%26+65;
     show+=String.fromCharCode(r);
     }
     return inputElement2.value=show;
-} */
+} 
 
-buttonCifrar.addEventListener('click', window.cipher.encode);
+//Agregando una funcion al evento click de los button
 
-buttonCifrar1.addEventListener('click', window.cipher.decode);
+buttonCifrar.addEventListener('click', cifrar);
+
+buttonCifrar1.addEventListener('click',  descifrar);
